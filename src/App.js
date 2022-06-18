52eedbc1019d25fe48ecb97a7a018ac931c7089f
Zuvo007 <duttasubhabrata123@gmail.com>
2022-06-18 16:50:29 +0530

    contact picker updated

--- src/App.js
+++ src/App.js
@@ -5,27 +5,35 @@ function App() {
   const [name, setName] = useState("test1");
   const [phone, setPhone] = useState("123");
 
-  const handleClick = async () => {
-    try {
-      const supported = "contacts" in navigator && "ContactsManager" in window;
-      //console.log(supported);
-      if (supported) {
-        const props = ["name", "phone"];
-        const opts = { multiple: false };
-        const contacts = await navigator.contacts.select(props, opts);
-        setName(contacts[0]);
-      }
-    } catch (err) {
-      throw err;
+  function openContactPicker() {
+    const supported = "contacts" in navigator && "ContactsManager" in window;
+
+    if (supported) {
+      getContacts();
+    } else {
+      alert(
+        "Contact list API not supported!. Only for android mobile chrome and chrome version > 80"
+      );
     }
-  };
+  }
+  async function getContacts() {
+    const props = ["name", "tel"];
+    const opts = { multiple: false };
+
+    try {
+      const contacts = await navigator.contacts.select(props, opts);
+      alert(JSON.stringify(contacts));
+    } catch (err) {
+      alert(err);
+    }
+  }
 
   return (
     <div className="App">
       <div>
         <div>Name: {name} </div>
         <div>Phone: {phone} </div>
-        <button className="mt-10" onClick={handleClick}>
+        <button className="mt-10" onClick={() => openContactPicker()}>
           Select contacts
         </button>
       </div>
