75bf6e31ad45fab5607c779e0345a1bb02fa6712
Zuvo007 <duttasubhabrata123@gmail.com>
2022-06-18 16:14:57 +0530

    contact picker added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("test1");
+  const [phone, setPhone] = useState("123");
+
+  const handleClick = async () => {
+    try {
+      const supported = "contacts" in navigator && "ContactsManager" in window;
+      //console.log(supported);
+      if (supported) {
+        const props = ["name", "phone"];
+        const opts = { multiple: false };
+        const contacts = await navigator.contacts.select(props, opts);
+        setName(contacts[0]);
+      }
+    } catch (err) {
+      throw err;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div>Name: {name} </div>
+        <div>Phone: {phone} </div>
+        <button className="mt-10" onClick={handleClick}>
+          Select contacts
+        </button>
+      </div>
     </div>
   );
 }
